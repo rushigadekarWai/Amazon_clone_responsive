73822ad9f780a9de4e6d15c751867e3b5b3ca36b
ruhigadekarWai <rushikesh.gadekar@waiin.com>
2025-03-05 12:51:47 +0530

    add backend practice

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,4 +1,7 @@
 import { rederOrderSummary } from "./Checkouts/orderSummary.js";
 import { renderPaymentSummary } from "./Checkouts/paymentSummary.js";
+import '../data/backend-practice.js';
+
+
 rederOrderSummary();
 renderPaymentSummary();
